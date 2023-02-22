9dfaab5fb7fcdd58f48596269452b9487cadda5d
BraianPerez97 <3603@holbertonstudents.com>
2023-02-22 02:13:31 +0000

    added string

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*
+*Description: print alphabet in lowercase
+*Return: Always 0.
+*/
+
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar (x);
+
+	}
+	putchar ('\n');
+	return (0);
+}
+
