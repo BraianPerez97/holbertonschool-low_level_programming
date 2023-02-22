4e6863e6f7bf5905631091632856f6f51468803f
BraianPerez97 <3603@holbertonstudents.com>
2023-02-22 02:26:02 +0000

    alphabet reverse

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+*main - Entry.
+*
+*Description: print the alphabet in reverse.
+*Return: Always 0.
+*/
+
+int main(void)
+{
+	char x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+
+		putchar (x);
+
+	}
+	putchar ('\n');
+	return (0);
+}
