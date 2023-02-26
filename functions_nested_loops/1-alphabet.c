00ab989746e065bf3f5769738911539e97f3f2ab
BraianPerez97 <3603@holbertonstudents.com>
2023-02-26 23:11:15 +0000

    Alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet - entry
+ *
+ * description - print alphabet 
+ * lowercase 
+ * return - void
+ */
+void print_alphabet(void)
+{
+char x 'a';
+while (x<= 'z')
+{
+_putchar(x);
+x++;
+}
+_putchar("\n");
+return;
+}
