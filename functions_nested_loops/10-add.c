279acc6c84571bca67fcdc98915cf75bba30a2c7
BraianPerez97 <3603@holbertonschool.com>
2023-02-27 16:17:08 +0000

    add

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*add- Entry
+*@a: first input
+*@b: second input
+*Description: add two int and return
+*Return: 0
+*/
+int add(int a, int b)
+{
+int c;
+c = a + b;
+return (c);
+}
