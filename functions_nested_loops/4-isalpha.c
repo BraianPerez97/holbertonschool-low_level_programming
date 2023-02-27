4bfd7936662fff7feb9fc06b03b347383585ec63
BraianPerez97 <3603@holbertonschool.com>
2023-02-27 16:36:17 +0000

    betty

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- *_isalpha - entry
- *@c: character to be checked
- *Description: check alphabetic character
- *return: 0
- */
+*_isalpha - Entry
+*@c: input
+*Description: check lowercase characters.
+*Return: ()
+*/
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
