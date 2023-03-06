b66bb1b2263336726e81298e9c3e06b0ce3e9c29
BraianPerez97 <3603@holbertonstudents.com>
2023-03-06 20:33:04 +0000

    puts2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -16,7 +16,7 @@ void rev_string(char *s)
 	for (y = 0; y < x; y++)
 	{
 		x--;
-		a =s[y];
+		a = s[y];
 		s[y] = s[x];
 		s[x] = a;
 	}
--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,30 @@
-include "main.h"
+#include "main.h"
 /**
  * puts2 - entry.
  * description: print every other character.
  * @x: input
  * return: print.
  */
-void puts2(char *str);
+void rev_string(char *s)
 {
-	int i = 0;
+	int x = 0, y, z;
+	char *str, temp;
 
-	for (; str[i] != '\0'; i++)
+	while (x >= 0)
 	{
-		if ((i % 2) == 0)
-			_putchar(str[i]);
-		else
-			continue;
+		if (s[x] == '\0')
+			break;
+		x++;
 	}
-	_putchar('\n');
+	str = s;
+
+	for (y = 0; y < (x - 1); y++)
+	{
+		for (z = y + 1; z > 0; z--)
+		{
+			temp = *(str + z);
+			*(str + z) = *(str + (z - 1));
+			*(str + (z - 1)) = temp;
+		}
+	}
 }
