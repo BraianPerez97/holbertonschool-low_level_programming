c5369cb920de522798360d1a04c980685abbe5cc
BraianPerez97 <3603@holbertonstudents.com>
2023-03-06 18:32:09 +0000

    multiply

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * _isdigit - checks digits 0-9
- * @c: input
- * return: 1 if c is a digit, 0 if otherwise.
+ *_isdigit - checks digits 0-9
+ *@c: input
+ *return: 1 if c is a digit, 0 if otherwise.
  */
+
 int _isdigit(int c)
 {
-	if ((c >= 48) && (c <= 57))
-		return (1);
-	else
-		return (0);
+if ((c >= 48) && (c <= 57))
+return (1);
+else
+return (0);
 }
---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul(int a, int b) - multiply to int
+ * @a: first int
+ * @b: second int
+ * return: result of multiplication.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
