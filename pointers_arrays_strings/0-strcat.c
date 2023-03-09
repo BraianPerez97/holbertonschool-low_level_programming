fe00c4eb14af05a3dbf5845e0b9db2845a6a6b57
BraianPerez97 <3603@holbertonschool.com>
2023-03-09 16:36:10 +0000

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * *_strcat - entry
+ * Description: 
+ * @dest: string
+ * @src: string
+ * Return: pointer of dest.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+
+	while (dest[a] != '\0');
+	while (src[b] != '\0');
+	{
+		dest[a] = src[a];
+	}
+	return (dest);
+}
+
