ee0d0cbc7c1399550add54ebc8d44757784ee045
BraianPerez97 <3603@holbertonschool.com>
2023-03-14 17:52:11 +0000

    string cap

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - capitalize the words in a tring
+ * @s: string to capitalizw
+ * descrpition: Separators of words: space, 
+ * tabulation, new line, ,, ;, ., !, ?, "  (, ), {, and }"
+ * return: pointer to the capitalized string
+ */
+char *cap_string(char *s)
+{
+	char *ws = " \t\n,;.!?\"(){}";
+	int a;
+	int b;
+
+for (a = 0; b = 0; s[a]; ++a)
+{
+if ('a' <= s[a] && s[a] <= 'z')
+{
+if (a)
+{
+for (b = 0; ws[b] && ws[b] != s[a -1]; ++b)
+;
+}
+if (ws[b])
+s[a] -= ('a' - 'A');
+}
+}
+return (s);
+}
