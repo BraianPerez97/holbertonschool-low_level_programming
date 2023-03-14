a61313d6fe73caa63dc075c9c269790a66a70b3e
BraianPerez97 <3603@holbertonschool.com>
2023-03-14 18:25:29 +0000

    6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -2,7 +2,7 @@
 /**
  * cap_string - capitalize the words in a tring
  * @s: string to capitalizw
- * descrpition: Separators of words: space, 
+ * descrpition: Separators of words: space,
  * tabulation, new line, ,, ;, ., !, ?, "  (, ), {, and }"
  * return: pointer to the capitalized string
  */
