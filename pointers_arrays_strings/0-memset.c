cb8492b8890950045abf05268c2782f679ddd82e
BraianPerez97 <3603@holbertonschool.com>
2023-03-14 17:23:54 +0000

    fixxing typos

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - fills memory with constant byte
+ * @n: buffer size
+ * @s: bytes of pointed memory area
+ * @b: the constant byte
+ * return: pointer memory area s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigined int i;
+
+	for (i = 0; i < n; i++)
+	{
+		*(s +i) = b;
+	}
+	return (s);
+}
--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -5,7 +5,7 @@
  * @s2: second string
  * Return: result
  */
-int _stncmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
