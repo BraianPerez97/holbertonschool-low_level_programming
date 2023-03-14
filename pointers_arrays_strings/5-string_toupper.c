bcfeb3ecb6a3e9cc5e8d3dd9e4e16077fe252cdd
BraianPerez97 <3603@holbertonschool.com>
2023-03-14 17:28:59 +0000

    update 5 main

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
  * string_tpupper - changes all lowercase letters
- * @s: sting 
+ * @s: sting
  * return: uppercase
  */
 char *string_toupper(char *s)
 	{
 		int x = 0;
+
 		while (*(s + x))
 	{
 		if ((*(s + x) >= 97) && (*(s + x) <= 122))
