b93eff010692d75457e7bcf1ba7bb3f4acd3b8fa
BraianPerez97 <3603@holbertonschool.com>
2023-03-24 20:09:36 +0000

    dog

---
+++ dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new structure type
+ * @name: char
+ * @age: float
+ * @owner: char
+ * description: define new type
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
