31072161d7083dc05cdaef0c530df359d5213f3d
BraianPerez97 <3603@holbertonschool.com>
2023-03-24 18:42:24 +0000

    main

--- preprocessor/2-main.c
+++ preprocessor/2-main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-#ifdef __FILE__
-#endif
 /**
  * main - entry
  * return: 0
  */
 int main (void)
 {
-	printf("__FILE__%s\n", __FILE__);
+	printf("%s\n", __FILE__);
 	return 0;
 }
