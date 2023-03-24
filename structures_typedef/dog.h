884bb07f0b507e98228f0f86ab100192b46b6450
BraianPerez97 <3603@holbertonschool.com>
2023-03-24 20:11:40 +0000

    dog

---
+++ structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - new structure type
+ * @name: char
+ * @age: float
+ * @owner: char
+ * description: define new type
+ */
+struct dog
+{
+        char *name;
+        float age;
+        char *owner;
+}
+~                                                                                                 
+~                                                                                                 
+~                                                                                                 
+
