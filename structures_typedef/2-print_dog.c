c66311d9ef2485aae060a7b520a8c08d772dbad9
BraianPerez97 <3603@holbertonschool.com>
2023-03-27 14:06:03 +0000

    betty

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -6,23 +6,31 @@
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL) {
-        return;
-    }
+if (d == NULL)
+{
+return;
+}
 
-    printf("Name: ");
-    if (d->name == NULL) {
-        printf("(nil)\n");
-    } else {
-        printf("%s\n", d->name);
-    }
+printf("Name: ");
+if (d->name == NULL)
+{
+printf("(nil)\n");
+}
+else
+{
+printf("%s\n", d->name);
+}
 
-    printf("Age: %f\n", d->age);
+printf("Age: %f\n", d->age);
 
-    printf("Owner: ");
-    if (d->owner == NULL) {
-        printf("(nil)\n");
-    } else {
-        printf("%s\n", d->owner);
-    }
+printf("Owner: ");
+if (d->owner == NULL)
+{
+printf("(nil)\n");
+
+}
+else
+{
+printf("%s\n", d->owner);
+}
 }
