4d7a7efa4c77573de085f7b864cbfd8c7dc9ba9a
BraianPerez97 <3603@holbertonschool.com>
2023-03-28 16:41:45 +0000

    fixed code

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 #include <stdio.h>
 /**
  * malloc_checked - function that allocates memory using malloc
@@ -7,11 +8,11 @@
  */
 void *malloc_checked(unsigned int b)
 {
-	void *ptr - malloc(b);
+	void *ptr;
+
+	ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
-		exit(98);
-	}
-	return *ptr;
+	exit(98);
+	return (ptr);
 }
