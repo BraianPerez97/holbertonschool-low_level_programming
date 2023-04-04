a97a07ee0dc39bd0522fd4ea30c875321a6898ab
BraianPerez97 <3603@holbertonschool.com>
2023-04-04 23:41:27 +0000

    initial commit with readme file

--- holbertonschool-printf/_printf.c
+++ holbertonschool-printf/_printf.c
@@ -8,39 +8,39 @@
 
 int _printf(const char *format, ...)
 {
-int counter = 0;
-va_list args;
-va_start(args, format);
+	int counter = 0;
+	va_list args;
+	va_start(args, format);
 
-while (*format)
-{
-if (*format == '%')
-{
-format++;
-switch (*format)
-{
-case 'c':
-counter += _putchar(va_arg(args, int));
-break;
-case 's':
-counter += puts(va_arg(args, char *));
-break;
-case '%':
-counter += _putchar('%');
-break;
-default:
-counter += _putchar('%');
-counter += _putchar(*format);
-break;
-}
-}
-else
-{
-counter += _putchar(*format);
-}
-format++;
-}
+	while (*format)
+	{
+		if (*format == '%')
+		{
+			format++;
+			switch (*format)
+			{
+				case 'c':
+					counter += _putchar(va_arg(args, int));
+					break;
+				case 's':
+					counter += puts(va_arg(args, char *));
+					break;
+				case '%':
+					counter += _putchar('%');
+					break;
+				default:
+					counter += _putchar('%');
+					counter += _putchar(*format);
+					break;
+			}
+		}
+		else
+		{
+			counter += _putchar(*format);
+		}
+		format++;
+	}
 
-va_end(args);
-return (counter);
+	va_end(args);
+	return (counter);
 
---
+++ singly_linked_lists/README.md
@@ -0,0 +1,9 @@
+C- Singly Linked List
+
+What is a linked list?
+
+A linked list is a linear data structure. Every linked list has two parts.
+1. data section 
+2. Address section (Holds the address of the next element in the list)
+	That element is called a "Node"
+
