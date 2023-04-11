e7737c7da0db03e78ab875d20e379bb011c2a7f4
BraianPerez97 <3603@holbertonschool.com>
2023-04-11 23:46:07 +0000

    add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,1 +1,42 @@
-#include "list.h"
+#include "lists.h"
+/**
+ * _strlen - returns the length of a string.
+ * @s: pointer to String
+ * Return: Nothing
+ */
+int _strlen(const char *s)
+{
+	int i = 0;
+
+	while (*(s + i) != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
+/**
+ * add_node - function that adds node
+ * @head: head of the list
+ * @str: String of the instance
+ * Return: new list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = malloc(sizeof(list_t));
+	if (new_node != NULL)
+	{
+		new_node->str = strdup(str);
+		new_node->len = _strlen(str);
+		new_node->next = *head;
+		*head = new_node;
+	} 
+	else
+	{
+		return (NULL);
+	}
+	*head = new_node;
+	return (new_node);
+}
