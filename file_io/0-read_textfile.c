bf3d3538a2d0d8132245fb2d9b9f27d8e5ea7a3f
BraianPerez97 <3603@holbertonschool.com>
2023-04-14 21:33:42 +0000

    sucessfully compilled

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -9,6 +9,41 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int file_descriptor = -1;
-	ssize_t output = 0;
+	int file_des;
 	char *buffer;
+	ssize_t text_read_des, text_write_des;
+
+	if (!filename)
+		return (0);
+
+	file_des = open(filename, O_RDONLY);
+	if (file_des == -1)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (!buffer)
+	{
+		close(file_des);
+		return (0);
+	}
+
+	text_read_des = read(file_des, buffer, letters);
+	if (text_read_des == -1)
+	{
+		close(file_des);
+		free(buffer);
+		return (0);
+	}
+
+	text_write_des = write(STDOUT_FILENO, buffer, text_read_des);
+	if (text_write_des == -1 || text_write_des != text_read_des)
+	{
+		close(file_des);
+		free(buffer);
+		return (0);
+	}
+
+	close(file_des);
+	free(buffer);
+	return (text_write_des);
+}
