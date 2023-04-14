24cc859361b087bf6db49a135f5454a7ac268bf3
BraianPerez97 <3603@holbertonschool.com>
2023-04-14 17:40:56 +0000

    hash

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+#include <stddef.h>
+/**
+ * has_table_create - creats a hash table 
+ * @size: is the array size
+ * Return: a pointer to a new table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *new_table;
+
+	if (size > 0)
+	{
+		new_table = malloc(sizeof(hash_table_create));
+		if (!new_table)
+			return (NULL);
+		new_table->size = size;
+		new_table->array = malloc(sizeof(char *) * size);
+		if (!new_table->array)
+			return (NULL);
+	}
+
+	return (new_table);
+}
--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,3 +1,15 @@
+#ifdef HASH_TABLE
+#define HASH_TABLE
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <stdef.h>
+
 hash_table_t *hash_table_create(unsigned long int size);
 unsigned long int hash_djb2(const unsigned char *str);
 unsigned long int key_index(const unsigned char *key, unsigned long int size);
@@ -5,3 +17,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 void hash_table_print(const hash_table_t *ht);
 void hash_table_delete(hash_table_t *ht);
+
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node;
+
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+#endif
