1c73c2220e968309f347f17fa4fc8c22d2f0441b
BraianPerez97 <3603@holbertonschool.com>
2023-08-04 22:43:01 +0000

    adding advanced

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -4,8 +4,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <math.h>
 
 /*----------PROTOTYPES----------*/
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+
 #endif
